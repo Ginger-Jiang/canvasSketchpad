f99e714c6a4736015468bd138a266f96cecbd6c2
Ginger <ziqingnanyi@gmail.com>
2019-06-19 12:48:21 +0800

    add color select

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var ctx = canvas.getContext('2d')
 autoSetCanvasSize(canvas)
 listenToUser(canvas)
 
-function drawLine(oldX, oldY, newX, newY) {
+
+// 画线
+function drawLine (oldX, oldY, newX, newY) {
   ctx.lineWidth = 10
   ctx.beginPath()
   ctx.moveTo(oldX, oldY)
@@ -15,40 +17,73 @@ function drawLine(oldX, oldY, newX, newY) {
   ctx.closePath()
 }
 
+// 颜色选择器
+var red = findElement('red')
+var green = findElement('green')
+var blue = findElement('blue')
+red.onclick = function(){
+  ctx.strokeStyle = 'red'
+  red.classList.add('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+}
+green.onclick = function(){
+  ctx.strokeStyle = 'green'
+  red.classList.remove('active')
+  green.classList.add('active')
+  blue.classList.remove('active')
+}
+blue.onclick = function(){
+  ctx.strokeStyle = 'blue'
+  red.classList.remove('active')
+  green.classList.remove('active')
+  blue.classList.add('active')
+}
+
+
+// 元素选择器
+function findElement (eleId) {
+  return document.getElementById(eleId)
+}
+
 // 橡皮擦
 var eraserEnable = false
-var actions = document.getElementById('actions')
-var eraser = document.getElementById('eraser')
-var brush = document.getElementById('brush')
-eraser.onclick = function() {
+var eraser = findElement('eraser')
+var brush = findElement('brush')
+eraser.onclick = function () {
   eraserEnable = true
-  actions.className = 'actions x'
+  eraser.classList.add('active')
+  brush.classList.remove('active')
 }
-brush.onclick = function() {
+brush.onclick = function () {
   eraserEnable = false
-  actions.className = 'actions'
+  brush.classList.add('active')
+  eraser.classList.remove('active')
 }
+
+
+
 // 自动设置canvas大小
-function autoSetCanvasSize(canvas) {
+function autoSetCanvasSize (canvas) {
   setCanvasSize()
 
-  function setCanvasSize() {
+  function setCanvasSize () {
     canvas.width = document.documentElement.clientWidth
     canvas.height = document.documentElement.clientHeight
   }
 
-  window.onresize = function() {
+  window.onresize = function () {
     setCanvasSize()
   }
 }
 
 // 监听鼠标动作
-function listenToUser(canvas) {
+function listenToUser (canvas) {
   var flag = false
   var lastPoint
-
-  if (document.body.ontouchstart === 'undefined') {
-    canvas.onmousedown = function(e) {
+  if (document.body.ontouchstart === undefined) {
+    // PC
+    canvas.onmousedown = function (e) {
       var x = e.clientX
       var y = e.clientY
       flag = true
@@ -62,7 +97,7 @@ function listenToUser(canvas) {
       }
     }
 
-    canvas.onmousemove = function(e) {
+    canvas.onmousemove = function (e) {
       var x = e.clientX
       var y = e.clientY
       if (!flag) return
@@ -83,12 +118,12 @@ function listenToUser(canvas) {
       }
     }
 
-    canvas.onmouseup = function(e) {
+    canvas.onmouseup = function (e) {
       flag = false
     }
   } else if (document.body.ontouchstart === null) {
-    canvas.ontouchstart = function(e) {
-      console.log(e)
+    // mobile
+    canvas.ontouchstart = function (e) {
       var x = e.touches[0].clientX
       var y = e.touches[0].clientY
       flag = true
@@ -102,7 +137,7 @@ function listenToUser(canvas) {
       }
     }
 
-    canvas.ontouchmove = function(e) {
+    canvas.ontouchmove = function (e) {
       var x = e.touches[0].clientX
       var y = e.touches[0].clientY
       if (!flag) return
@@ -123,7 +158,7 @@ function listenToUser(canvas) {
       }
     }
 
-    canvas.ontouchend = function(e) {
+    canvas.ontouchend = function (e) {
       flag = false
     }
   }
